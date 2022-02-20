1a55f872e95a546e6302915d7ff1922204995e53
mygonia <mariusygonia@gmail.com>
2022-02-20 01:39:40 -0500

    Removed sidenav

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main id="sidenav">
+      <main>
         <Header title="Welcome to my app!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
