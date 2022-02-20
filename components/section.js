cf8aa7f858d76f741a142152be6ac7ff141ca45e
mygonia <mariusygonia@gmail.com>
2022-02-20 08:45:48 -0500

    Update section.js

--- components/section.js
+++ components/section.js
@@ -15,7 +15,7 @@ export default function FirstPost({ title, nextpage }) {
                 <Link href="/">
                     <a>Back to Home</a>
                 </Link> | 
-                <Link href={"/sections/" + nextpage}>
+                <Link href={"sections/" + nextpage}>
                     <a>{nextpage}</a>
                 </Link>
             </h2>
