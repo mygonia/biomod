055424abd0032bc1352500fd5ce1496390f7e39d
mygonia <mariusygonia@gmail.com>
2022-02-20 07:10:33 -0500

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,6 @@ import styles from './Footer.module.css'
 export default function Footer() {
   return (
     <>
-      <img id="shouldersOfGiants" src="/images/shouldersOfGiants.png"></img>
       <footer className={styles.footer}>
         Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> from  <em>McMaster University in Hamilton, Ontario</em>  for you
       </footer>
