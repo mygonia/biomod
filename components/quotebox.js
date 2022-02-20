849e984f7fbc7c01d4a94e4106b55562698a72c2
mygonia <mariusygonia@gmail.com>
2022-02-20 06:22:55 -0500

    A

--- components/quotebox.js
+++ components/quotebox.js
@@ -1,9 +1,9 @@
-import styles from './quotebox.module.css'
+
 
 export default function quotebox({ quote, author }) {
     return (
         <>
-            <div className={styles.animated-border-quote}>
+            <div >
                 <blockquote>
                     <p>{quote}</p>
                     <cite>{author}</cite>
