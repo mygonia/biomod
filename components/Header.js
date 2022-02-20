ac3c3d381c933d03e4f3a94298b2d9ca9766459e
mygonia <mariusygonia@gmail.com>
2022-02-20 00:55:16 -0500

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,25 @@
 export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
+  return (
+  <>
+    <h1 className="title">{title}</h1>
+    <ul>
+      <li>Abstract</li>
+      <li>Introduction</li>
+        <ul>
+          <li>Background</li>
+          <li>Problem</li>
+        </ul>
+      <li>Solution</li>
+        <ul>
+          <li>Summary of Findings</li>
+          <li>Mechanism</li>
+          <li>caDNAno Simulations</li>
+        </ul>
+      <li>Materials and Methods</li>
+      <li>Discussion</li>
+      <li>Future</li>
+      <li>Team</li>
+    </ul>
+  </>
+  )
 }
