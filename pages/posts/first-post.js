f72ec7629d86eb12a6a63be59c751633f1387e68
mygonia <mariusygonia@gmail.com>
2022-02-19 20:52:29 -0500

    Minor changes

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,18 @@
 import Link from 'next/link'
+import Image from 'next/image'
+
+const YourComponent = () => (
+    <Image
+        src="/images/profile.png"
+        alt="Holding a Javascript textbook"
+    />
+)
 
 export default function FirstPost() {
     return (
         <>
             <h1>First Post</h1>
+            <YourComponent></YourComponent>
             <h2>
                 <Link href="/">
                     <a>Back to home</a>
