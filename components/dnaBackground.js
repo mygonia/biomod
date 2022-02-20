ba79573bf9594e8b2e4c9b65d021beb077dd3231
mygonia <mariusygonia@gmail.com>
2022-02-20 04:26:13 -0500

    So close

--- components/dnaBackground.js
+++ components/dnaBackground.js
@@ -3,9 +3,7 @@ import Script from 'next/script'
 export default function dnaBackground() {
     return (
         <>
-            <Script
-                src="https://cdnjs.cloudflare.com/ajax/libs/three.js/86/three.min.js"
-            />
+            <script src="/static/three.min.js"></script>>
             <script src="/static/script2.js"></script>
             <script src="/static/script.js"></script>
         </>
