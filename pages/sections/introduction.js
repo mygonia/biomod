5a4e4c30b7893c00b514781801ca7421ba6e8773
mygonia <mariusygonia@gmail.com>
2022-02-20 08:03:08 -0500

    Create introduction.js

---
+++ pages/sections/introduction.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import Section from '../../components/section'
+
+export default function FirstPost() {
+    return (
+        <Section title="Introduction" nextpage="Background">
+
+        </Section>
+    )
+}
