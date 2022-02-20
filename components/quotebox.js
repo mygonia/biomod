4daf313b854470baa06c06edea058a8061ecfd0e
mygonia <mariusygonia@gmail.com>
2022-02-20 05:17:46 -0500

    Fixed quotebox

--- components/quotebox.js
+++ components/quotebox.js
@@ -2,7 +2,7 @@ import styles from './quotebox.module.scss'
 
 export default function quotebox({ quote, author }) {
     return (
-        <div class="animated-border-quote">
+        <div className={styles.animated-border-quote}>
             <blockquote>
                 <p>{quote}</p>
                 <cite>{author}</cite>
