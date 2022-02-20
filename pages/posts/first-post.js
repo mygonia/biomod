2990678fdd8c45ce15f01668912c5bb233ba201b
mygonia <mariusygonia@gmail.com>
2022-02-19 21:33:37 -0500

    Update first-post.js

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import Head from 'next/head'
+import Script from 'next/script'
 
 const YourComponent = () => (
     <Image
@@ -13,6 +15,16 @@ const YourComponent = () => (
 export default function FirstPost() {
     return (
         <>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <Script>
+                src="https://connect.facebook.net/en_US/sdk.js"
+                strategy="lazyOnload"
+                onLoad={() =>
+                    console.log(`script loaded correctly, window.FB has been populated`)
+                }
+            </Script>
             <h1>First Post</h1>
             <YourComponent></YourComponent>
             <h2>
