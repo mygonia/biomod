b732f3947466ce3815d7069409fa97c820941d9a
mygonia <mariusygonia@gmail.com>
2022-02-20 01:07:46 -0500

    Added links

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,26 @@
+import Link from 'next/link'
+
 export default function Header({ title }) {
   return (
   <>
     <h1 className="title">{title}</h1>
     <ul>
-      <li>Abstract</li>
-      <li>Introduction</li>
+      <li>
+        <Link href="/sections/abstract.js">
+          <a>Abstract</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/sections/introduction.js">
+          <a>Introduction</a>
+        </Link>
+      </li>
         <ul>
-          <li>Background</li>
+          <li>
+            <Link href="/sections/background.js">
+              <a>Background</a>
+            </Link>
+          </li>
           <li>Problem</li>
         </ul>
       <li>Solution</li>
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 import Link from 'next/link'
-import '@styles/globals.css'
 
 export default function Home() {
   return (
