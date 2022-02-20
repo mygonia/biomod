275aa778f2ff67b52539425a5e098dd454d737cf
mygonia <mariusygonia@gmail.com>
2022-02-20 01:10:44 -0500

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -6,35 +6,43 @@ export default function Header({ title }) {
     <h1 className="title">{title}</h1>
     <ul>
       <li>
-        <Link href="/sections/abstract.js">
+        <Link href="/sections/abstract">
           <a>Abstract</a>
         </Link>
       </li>
       <li>
-        <Link href="/sections/introduction.js">
+        <Link href="/sections/introduction">
           <a>Introduction</a>
         </Link>
       </li>
         <ul>
           <li>
-            <Link href="/sections/background.js">
+            <Link href="/sections/background">
               <a>Background</a>
             </Link>
           </li>
           <li>
-            <Link href="/sections/problem.js">
+            <Link href="/sections/problem">
               <a>Problem</a>  
             </Link>
           </li>
         </ul>
       <li>
-        <Link href="/sections/solution.js">
+        <Link href="/sections/solution">
           <a>Solution</a>
         </Link>
       </li>
         <ul>
-          <li>Summary of Findings</li>
-          <li>Mechanism</li>
+          <li>
+            <Link href="/sections/summary">
+              <a>Summary of Findings</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/sections/mechanism">
+              <a>Mechanism</a>
+            </Link>
+          </li>
           <li>caDNAno Simulations</li>
         </ul>
       <li>Materials and Methods</li>
