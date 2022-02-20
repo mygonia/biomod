326630759ed0fbc5874c81e4be83edcc14a4900f
mygonia <mariusygonia@gmail.com>
2022-02-20 07:20:54 -0500

    Changes

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import Script from 'next/script'
 import Image from 'next/image'
 import Dnabackground from '@components/dnaBackground'
 import Quotebox from '@components/quotebox'
+import { motion } from 'framer-motion';
 
 export default function Home() {
   return (
@@ -16,16 +17,30 @@ export default function Home() {
       </Head>
 
       <Dnabackground></Dnabackground>
-      <img id="shouldersOfGiants" src="/images/shouldersOfGiants.png"></img>
+      <motion.div initial="hidden" animate="visible" variants={{
+        hidden: {
+          scale: .8,
+          opacity: 0
+        },
+        visible: {
+          scale: 1,
+          opacity: 1,
+          transition: {
+            delay: .4
+          }
+        },
+      }}>
+        <img id="shouldersOfGiants" src="/images/shouldersOfGiants.png"></img>
+      </motion.div>
       <main>
         <Header title="[Title of BIOMOD Project]" />
         <p className="description">
           Some interesting <code>tagline</code>
         </p>
         <h1 className="title">
-          Read{' '}
+          Watch{' '}
           <Link href="/posts/first-post">
-            <a>this page!</a>
+            <a>our video!</a>
           </Link>
         </h1>
         <Quotebox quote="If I have seen further it is by standing on the shoulders of Giants." author="Isaac Newton"/>
